271f3ab0d62eb0f1ce86035759ac3aeb4b6c0c30
Edward Harriss <harriss.edward24@gmail.com>
2021-02-14 01:00:43 +0000

    Temp maths showing funky answers

--- embedvid-19.py
+++ embedvid-19.py
@@ -1,6 +1,6 @@
 import board
 import busio
-import adafruit_tmp006
+import smbus2
 import adafruit_mlx90393
 import adafruit_vl53l0x
 import time
@@ -8,7 +8,6 @@ from gpiozero import Button, Buzzer
 import math
 
 i2c = busio.I2C(board.SCL, board.SDA)
-sensor_temp = adafruit_tmp006.TMP006(i2c)
 sensor_mag = adafruit_mlx90393.MLX90393(i2c, gain=adafruit_mlx90393.GAIN_1X)
 sensor_tof = adafruit_vl53l0x.VL53L0X(i2c)
 button = Button(10)
@@ -24,6 +23,42 @@ class User:
 
     def get_values(self):
         return [self.x, self.y, self.z, self.d]; 
+
+def get_temp():
+    bus = smbus2.SMBus(1)
+    meas_vobj = smbus2.i2c_msg.write(0x40,[0x00])
+    bus.i2c_rdwr(meas_vobj)
+    time.sleep(0.1)
+    read_result = smbus2.i2c_msg.read(0x40,2)
+    bus.i2c_rdwr(read_result)
+    Vobj_bin = int.from_bytes(read_result.buf[0]+read_result.buf[1],'big')
+    Vobj = Vobj_bin*(0.00000015625)
+    
+    meas_tdie = smbus2.i2c_msg.write(0x40,[0x01])
+    bus.i2c_rdwr(meas_tdie)
+    time.sleep(0.1)
+    read_result = smbus2.i2c_msg.read(0x40,2)
+    bus.i2c_rdwr(read_result)
+    Tdie = ((int.from_bytes(read_result.buf[0]+read_result.buf[1],'big') / 4)*0.03125)+273.14
+ 
+    b0 = -0.0000294
+    b1 = -0.00000057
+    b2 = 0.00000000463
+    Tref = 298.15
+    c2 = 13.4
+    S0 = 0.000000000000063
+    a1 = 0.00175
+    a2 = -0.00001678
+
+    Vos = b0 + b1*(Tdie-Tref) + b2*math.pow((Tdie-Tref),2.0)
+    print("Vos : ", Vos)
+    FVobj = (Vobj-Vos) + c2*math.pow((Vobj-Vos),2.0)
+    print("FVobj : ", FVobj)
+    S = S0*(1 + a1*(Tdie-Tref) + a2*math.pow((Tdie-Tref),2.0))
+    print("S : ", S)
+    Tobj = math.sqrt(math.sqrt(math.pow(Tdie,4.0) + (FVobj/S))) - 273.15
+    print(Tobj)
+
 
 def reset():
     mx, my, mz = sensor_mag.magnetic
@@ -67,6 +102,7 @@ time_scale = time.time()
 number = 1
 distance_ave = 0
 angle_ave = 0
+get_temp()
 
 while True:
     mx, my, mz = sensor_mag.magnetic
