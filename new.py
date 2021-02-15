21dc871f4df42984d6d31333b59fa525a27552c4
m4tt-v <vottamatthew@outlook.com>
2021-02-15 23:32:39 +0000

    receiveing values, not the correct ones though

--- new.py
+++ new.py
@@ -7,22 +7,23 @@ bus = smbus2.SMBus(1)
 i2c = busio.I2C(board.SCL, board.SDA)
 
 while True:
-	m = smbus2.i2c_msg.write(0x29, [0x30, 0x0E])
+	m = smbus2.i2c_msg.write(0x29, 0x30)
 	bus.i2c_rdwr(m)
 	time.sleep(1.0)
 	print("read/write")
-	readm = smbus2.i2c_msg.read(0x29, 0x0E) 
+	readm = smbus2.i2c_msg.read(0x29, 6) 
 	bus.i2c_rdwr(readm)
 	print(readm)
-	print(readm.buf[1], "x")
-	print(readm.buf[2], "y")
-	print(readm.buf[3], "z")
-	xi = (readm.buf[0] << 8) | readm.buf[1]
-	yi = (readm.buf[2] << 8) | readm.buf[3]
-	zi = (readm.buf[4] << 8) | readm.buf[5]
-	xi = xi - 0x8000	
-	yi = yi - 0x8000
-	zi = zi - 0x8000
-	print("X = ", xi, "Y = ", yi, "Z = ", zi)
+	x = int.from_bytes(readm.buf[0]+readm.buf[1],'big')
+	y = int.from_bytes(readm.buf[2]+readm.buf[3],'big')
+	z = int.from_bytes(readm.buf[4]+readm.buf[5],'big')
+	print("x  = ", x, "y = ", y, "z = ",z) 
+	#xi = (readm.buf[0] << 8) | readm.buf[1]
+	#yi = (readm.buf[2] << 8) | readm.buf[3]
+	#zi = (readm.buf[4] << 8) | readm.buf[5]
+	#xi = xi - 0x8000	
+	#yi = yi - 0x8000
+	#zi = zi - 0x8000
+	#print("X = ", xi, "Y = ", yi, "Z = ", zi)
 	time.sleep(1.0)	
 
