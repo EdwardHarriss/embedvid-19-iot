bcbfc681fa0007c48263e73d0e16eae10b885d79
m4tt-v <vottamatthew@outlook.com>
2021-02-18 17:33:31 +0000

    change register

--- new.py
+++ new.py
@@ -4,16 +4,19 @@ import board
 import smbus2
 
 bus = smbus2.SMBus(1)
+
 i2c = busio.I2C(board.SCL, board.SDA)
 
 while True:
-	m = smbus2.i2c_msg.write(0x29, 0x30)
+	m = smbus2.i2c_msg.write(0x0c, [0x0E])
 	bus.i2c_rdwr(m)
 	time.sleep(1.0)
+	#rm = smbus2.i2c_msg.write(0x0c, [0x40])
+	#bus.i2c_rdwr(rm)
+	time.sleep(1.0)
 	print("read/write")
-	readm = smbus2.i2c_msg.read(0x29, 6) 
+	readm = smbus2.i2c_msg.read(0x0c, 6) 
 	bus.i2c_rdwr(readm)
-	print(readm)
 	x = int.from_bytes(readm.buf[0]+readm.buf[1],'big')
 	y = int.from_bytes(readm.buf[2]+readm.buf[3],'big')
 	z = int.from_bytes(readm.buf[4]+readm.buf[5],'big')
@@ -26,4 +29,5 @@ while True:
 	#zi = zi - 0x8000
 	#print("X = ", xi, "Y = ", yi, "Z = ", zi)
 	time.sleep(1.0)	
+
 
