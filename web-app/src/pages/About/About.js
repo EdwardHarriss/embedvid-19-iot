7b516381489faf8f580482a3bebc274a709d2beb
frg18 <frg18@ic.ac.uk>
2021-02-23 15:45:29 +0000

    About page

--- web-app/src/pages/About/About.js
+++ web-app/src/pages/About/About.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import ReactPlayer from 'react-player'
 import Header from '../.././components/Header/Header';
+import graphic from '../../images/productivity.jpg'
 import './About.css'
 
 class About extends Component {
@@ -7,6 +9,24 @@ class About extends Component {
     return (
       <div className="container">
         <Header page={'About'}/>
+        <h5>From the embedvid-19 team, we are proud to present to you, WorkMate!</h5>
+        <ReactPlayer 
+          className="ad-vid"
+          width="80%"
+          url="https://www.youtube.com/watch?v=gsCVpNI1cgU&feature=youtu.be&ab_channel=WorkMate"
+        />
+        <p><span className="top-para">WorkMate is the innovative new device which tracks the time you spend at your desk working and rewards you for meeting your daily target.</span></p>
+        <p>WorkMate can be used in many different scenarios to help increase productivity, happiness and life!</p>
+        <img src={graphic} alt="productivity graphic" id="graphic" className="graphic"></img>
+        <p>Not only does the device track time spent at your desk, but also your posture and room temperature, giving you the power to set up the optimal work environment.</p>
+        <p>You can view all of this handy data on the website!</p>
+        <p>WorkMate is a device built to help you succeed. WorkMate targets your working environment so you can work at 100%, 100% of the time.</p>
+        <h3>How To Use</h3>
+        <ReactPlayer 
+          className="demo-vid"
+          width="80%"
+          url="https://www.youtube.com/watch?v=gsCVpNI1cgU&feature=youtu.be&ab_channel=WorkMate"
+        />
       </div>
     );
   }
